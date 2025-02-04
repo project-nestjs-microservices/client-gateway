9430e8097659946c524b46ab7086b4e14e41cda0
Jhossuan <jhossuacamposd86@gmail.com>
2025-02-03 19:32:40 -0500

    log actualizado desde el launcher

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  console.log('Hola mundo! primer cambio...');
+  console.log('Hola mundo! segundo cambio...');
 
   logger.log(`Server running on port ${envs.port}`);
 }
