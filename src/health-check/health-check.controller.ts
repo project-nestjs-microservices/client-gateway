465e19c53ac5c9b7633d84f01852845ca1b33c85
Jhossuan <jhossuacamposd86@gmail.com>
2025-03-31 09:54:37 -0500

    new changes

---
+++ src/health-check/health-check.controller.ts
@@ -0,0 +1,11 @@
+import {Controller, Get} from '@nestjs/common';
+
+@Controller('/')
+export class HealthCheckController {
+
+    @Get()
+    healthCheck() {
+        return "Client gateway is up and running";
+    }
+
+}
